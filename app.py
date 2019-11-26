87b6387687b640f24f463fa592bb90da97965de2
fernando-bivar <54823255+fernando-bivar@users.noreply.github.com>
2019-11-26 01:07:38 -0300

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from sklearn.model_selection import train_test_split
 from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import accuracy_score
 from sklearn.neighbors import KNeighborsClassifier
-import matplotlib.pyplot as plt
 import numpy as np
 import json
 #import mglearn
